bb9c485f2a2ffc5bb76f8cd6fbcadbedafe72b00
Magnus Larsen <mlar@novozymes.com>
2023-01-23 07:42:12 +0100

    Only use exa if it is installed

--- dot_config/private_fish/executable_config.fish
+++ dot_config/private_fish/executable_config.fish
@@ -2,5 +2,13 @@ set -gx GPG_TTY (tty)
 fish_ssh_agent
 
 set -U fzf_preview_file_cmd preview
-set -U fzf_preview_dir_cmd exa -lg --color=always
+set -U fzf_preview_dir_cmd fzf_preview_dir_cmd_fun
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
+function fzf_preview_dir_cmd_fun
+	if command --query exa
+		exa -lg --color=always $argv
+	else
+		ls -l --color=always $argv
+	end
+end
