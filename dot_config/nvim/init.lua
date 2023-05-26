efbf03d69c16be711a3421d55a2688efe5e91292
Magnus Larsen <mlar@novozymes.com>
2023-05-26 10:31:14 +0200

    Fix the icons..

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -410,7 +410,16 @@ cmp.setup {
 	formatting = {
 		format = function(entry, vim_item)
 			-- Kind icons
-			return string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			-- Source
+			vim_item.menu = ({
+				buffer = "",
+				nvim_lsp = "",
+				luasnip = "",
+				nvim_lua = "",
+				latex_symbols = "",
+			})[entry.source.name]
+			return vim_item
 		end
 	},
 
