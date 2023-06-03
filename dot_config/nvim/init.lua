1db926c88d77deaf8316a00507f487c2f61f95c6
Magnus Larsen <magnusfynbo@hotmail.com>
2023-06-03 20:02:02 +0200

    add view all diagnostics keybind

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -260,20 +260,21 @@ local on_attach = function(_, bufnr)
 
 		keyset('n', keys, func, { buffer = bufnr, desc = desc })
 	end
+	local telescope = require('telescope.builtin')
 
 	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
+	nmap('da', telescope.diagnostics, '[D]iagnists List [A]ll')
+
 	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+	nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
 	nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-	nmap('gt', vim.lsp.buf.type_definition, 'Type [D]efinition')
-	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+	nmap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
 
 	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	nmap('gD', vim.lsp.buf.declaration, '[G]oto [^D]eclaration')
 end
 
 -- Enable the following language servers
