d0f786cccd10a211230126fa3529d788c59f8cf1
Magnus Larsen <mlar@novozymes.com>
2023-06-07 08:00:12 +0200

    change SQLParse with SqlFluff

--- dot_config/nvim/bin/executable_sql-format.py
+++ dot_config/nvim/bin/executable_sql-format.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-import sqlparse
 import sys
 
+import sqlfluff
 
 # TODO: Python: capture whole string (with `"`, format it to use """?
 #       That would also fix the one-line sql issue
 
+
 def main():
-    content = sys.stdin.read()
-    print(sqlparse.format(
-        content,
-        keyword_case="upper",
-        identifier_case="lower",
-        reindent=True,
-        reindent_aligned=False,
-        output_format="sql",
-        wrap_after=80,
-        comma_first=False,
-    ).strip())
+    c = sys.stdin.read()
+    print(
+        sqlfluff.fix(c, dialect="postgres", config_path=".sqlfluff").strip()
+    )
 
 
 if __name__ == "__main__":
