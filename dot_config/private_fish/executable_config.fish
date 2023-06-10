d968088651cfbab8924a863a6ff3b75d2e36fd4f
Magnus Larsen <magnusfynbo@hotmail.com>
2023-06-10 10:21:26 +0200

    cleanup bashrc + add alias for bat

--- dot_config/private_fish/executable_config.fish
+++ dot_config/private_fish/executable_config.fish
@@ -1,14 +1,20 @@
 set -gx GPG_TTY (tty)
 fish_ssh_agent
 
+# FZF stuff
+function fzf_preview_dir_cmd_fun
+    if command --query exa
+        exa -lg --color=always $argv
+    else
+        ls -l --color=always $argv
+    end
+end
+
 set -U fzf_preview_file_cmd preview
 set -U fzf_preview_dir_cmd fzf_preview_dir_cmd_fun
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 
-function fzf_preview_dir_cmd_fun
-	if command --query exa
-		exa -lg --color=always $argv
-	else
-		ls -l --color=always $argv
-	end
+# Aliases
+if type -q bat
+    alias cat="bat -p"
 end
