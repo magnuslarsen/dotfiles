1dfd05b892f3342da45bbf0aec29ded1c07cac83
Magnus Larsen <mlar@novozymes.com>
2023-07-06 12:58:14 +0200

    Fix the telescope ansible role detection

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,4 @@
-function Telescope_project_opts()
+local function telescope_project_opts()
 	local function is_ansible_repo()
 		local current_path = vim.fn.expand("%:p")
 		return string.match(current_path, "ansible")
@@ -6,7 +6,7 @@ function Telescope_project_opts()
 	local function get_ansible_role_path()
 		local current_path = vim.fn.expand("%:p")
 		if string.match(current_path, "role") then
-			return string.gsub(current_path, "^(.*/roles/[%a%-%_%.]*)/.*", "%1")
+			return string.gsub(current_path, "^(.*/roles/[%g]+)/.*/.*", "%1")
 		elseif string.match(current_path, "playbook") then
 			return string.gsub(current_path, "^(.*/playbooks/).*", "%1")
 		else
@@ -35,11 +35,11 @@ return {
 			},
 		},
 		keys = {
-			{ "fd",         function() require("telescope.builtin").find_files(Telescope_project_opts()) end },
+			{ "fd",         function() require("telescope.builtin").find_files(telescope_project_opts()) end },
 			{ "<leader>fd", function() require("telescope.builtin").find_files() end },
-			{ "rg",         function() require("telescope.builtin").live_grep(Telescope_project_opts()) end },
+			{ "rg",         function() require("telescope.builtin").live_grep(telescope_project_opts()) end },
 			{ "<leader>rg", function() require("telescope.builtin").live_grep() end },
-			{ "gw",         function() require("telescope.builtin").grep_string(Telescope_project_opts()) end },
+			{ "gw",         function() require("telescope.builtin").grep_string(telescope_project_opts()) end },
 			{ "<leader>gw", function() require("telescope.builtin").grep_string() end },
 			{ "<leader>rf", function() require("telescope.builtin").oldfiles() end },
 			{ "<leader>ht", function() require("telescope.builtin").help_tags() end },
