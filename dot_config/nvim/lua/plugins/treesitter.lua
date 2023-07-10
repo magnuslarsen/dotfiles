98790811803ec76ba0a189dc3fa5600c6734189d
Magnus Larsen <magnusfynbo@hotmail.com>
2023-07-10 09:06:58 +0200

    Added textobject keybinds + textsubjects

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,44 @@ return {
 		-- Load the treesitter extension, when treesitter is loaded
 		-- (even though they aren't dependencies)
 		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
+			{
+				'nvim-treesitter/nvim-treesitter-textobjects',
+				opts = {
+					textobjects = {
+						select = {
+							enable = true,
+							lookahead = true,
+							include_surrounding_whitespace = true,
+							keymaps = {
+								["af"] = "@function.outer",
+								["if"] = "@function.inner",
+								["ac"] = "@class.outer",
+								["ic"] = "@class.inner",
+								["ip"] = "@parameter.inner",
+								["ap"] = "@parameter.outer",
+							},
+						}
+					}
+				},
+				config = function(_, opts)
+					require("nvim-treesitter.configs").setup(opts)
+				end
+			},
+			{
+				'RRethy/nvim-treesitter-textsubjects',
+				opts = {
+					textsubjects = {
+						enable = true,
+						prev_seletion = ",",
+						keymaps = {
+							["."] = 'textsubjects-smart'
+						},
+					}
+				},
+				config = function(_, opts)
+					require("nvim-treesitter.configs").setup(opts)
+				end
+			},
 			'nvim-treesitter/nvim-treesitter-context',
 		},
 		opts = {
