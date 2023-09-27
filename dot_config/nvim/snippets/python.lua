56faf8308dee18a5b7c82bd68e412e6879d22e2c
Magnus Larsen <mlar@novozymes.com>
2023-09-27 09:16:00 +0200

    Cleanup python snippet; add go iferr snippet

--- dot_config/nvim/snippets/python.lua
+++ dot_config/nvim/snippets/python.lua
@@ -14,78 +14,21 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
-local snippets, autosnippets = {}, {}
-
-local group = vim.api.nvim_create_augroup("Python Snippets", { clear = true })
-local file_pattern = "*.py"
-
-local function cs(trigger, nodes, opts)
-	local snippet = s(trigger, nodes)
-	local target_table = snippets
-
-	local pattern = file_pattern
-	local keymaps = {}
-
-	if opts ~= nil then
-		-- check for custom pattern
-		if opts.pattern then
-			pattern = opts.pattern
-		end
-
-		-- if opts is a string
-		if type(opts) == "string" then
-			if opts == "auto" then
-				target_table = autosnippets
-			else
-				table.insert(keymaps, { "i", opts })
-			end
-		end
-
-		-- if opts is a table
-		if opts ~= nil and type(opts) == "table" then
-			for _, keymap in ipairs(opts) do
-				if type(keymap) == "string" then
-					table.insert(keymaps, { "i", keymap })
-				else
-					table.insert(keymaps, keymap)
-				end
-			end
-		end
-
-		-- set autocmd for each keymap
-		if opts ~= "auto" then
-			for _, keymap in ipairs(keymaps) do
-				vim.api.nvim_create_autocmd("BufEnter", {
-					pattern = pattern,
-					group = group,
-					callback = function()
-						vim.keymap.set(keymap[1], keymap[2], function()
-							ls.snip_expand(snippet)
-						end, { noremap = true, silent = true, buffer = true })
-					end,
-				})
-			end
-		end
-	end
-
-	table.insert(target_table, snippet) -- insert snippet into appropriate table
-end
-
 -- Snippets go here
-cs(
-	"ifmain",
-	fmt([[
+return {
+	s(
+		"ifmain",
+		fmt([[
 def {}():
 	{}{}
 
 if __name__ == '__main__':
 	{}()
 	]], {
-		i(1, "main"),
-		i(2, "pass"),
-		i(0),
-		rep(1),
-	})
-)
-
-return snippets, autosnippets
+			i(1, "main"),
+			i(2, "pass"),
+			i(0),
+			rep(1),
+		})
+	),
+}
