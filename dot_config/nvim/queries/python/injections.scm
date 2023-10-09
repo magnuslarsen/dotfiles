d7a3400905cace70a4a657c145d0c3a7e1db17ef
Magnus Larsen <mlar@novozymes.com>
2023-10-09 08:54:26 +0200

    Added morer injections for python; including JSON

--- dot_config/nvim/queries/python/injections.scm
+++ dot_config/nvim/queries/python/injections.scm
@@ -1,3 +1,4 @@
+; Function calls from common SQL libraries
 (expression_statement
   (call
     (attribute
@@ -13,3 +14,24 @@
   )
 )
 
+; Variables ending in "_sql" and "_SQL"
+(expression_statement
+  (assignment
+    left: (identifier) @_left (#match? @_left "(_sql|_SQL)$")
+    right: (string
+      (string_content) @injection.content
+      (#set! injection.language "sql")
+    )
+  )
+)
+
+; Variables ending in "_json" and "_JSON"
+(expression_statement
+  (assignment
+    left: (identifier) @_left (#match? @_left "(_json|_JSON)$")
+    right: (string
+      (string_content) @injection.content
+      (#set! injection.language "json")
+    )
+  )
+)
