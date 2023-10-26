b74d2fcc54393fc5e50961920fc70aef404d9216
Magnus Larsen <mlar@novozymes.com>
2023-10-26 07:45:39 +0200

    Rebinded some Flash keys

--- dot_config/nvim/lua/plugins/movement.lua
+++ dot_config/nvim/lua/plugins/movement.lua
@@ -6,14 +6,16 @@ return {
 			modes = {
 				char = {
 					jump_labels = true
+				},
+				search = {
+					enabled = false
 				}
 			}
 		},
 		keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end },
-			{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, },
-			{ "r", mode = "o",               function() require("flash").remote() end },
-			{ "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end },
+			{ "R", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, },
+			{ "r", mode = { "o", "x" },      function() require("flash").treesitter_search() end },
 		},
 	}
 }
