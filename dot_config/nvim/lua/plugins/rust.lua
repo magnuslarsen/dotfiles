057fffd0809a8e6547da465e4a66c2169110a823
Magnus Larsen <mlar@novozymes.com>
2023-11-05 15:16:45 +0100

    rust fixes

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -12,14 +12,6 @@ return {
 					cmp = { enabled = true },
 				},
 			})
-
-			vim.api.nvim_create_autocmd("BufRead", {
-				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-				pattern = "Cargo.toml",
-				callback = function()
-					require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-				end,
-			})
 		end,
 	},
 }
