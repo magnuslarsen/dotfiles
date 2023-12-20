a8d1ac3ef045239d59c2e145253077a68efee637
Magnus Larsen <magnusfynbo@hotmail.com>
2023-12-20 11:20:35 +0100

    Rust injections, and nvim-surround for Rust types

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -2,6 +2,21 @@ return {
 	{
 		"mrcjkb/rustaceanvim",
 		ft = { "rust" },
+		config = function()
+			-- Add custom surround function for types (overrides HTML "t")
+			local surround = require("nvim-surround")
+			local surround_cfg = require("nvim-surround.config")
+			surround.buffer_setup({
+				surrounds = {
+					["t"] = {
+						add = function()
+							local input = surround_cfg.get_input("Type: ")
+							return { { input .. "<" }, { ">" } }
+						end,
+					},
+				},
+			})
+		end,
 	},
 	{
 		"saecki/crates.nvim",
