412175387350582a3d1c3ae142e60cb49c6be54f
Magnus Larsen <magnusfynbo@hotmail.com>
2024-05-28 08:28:29 +0200

    [nvim]: fix the ansible command/shell injections; and add .cmd keys as well

--- dot_config/nvim/queries/yaml/injections.scm
+++ dot_config/nvim/queries/yaml/injections.scm
@@ -2,72 +2,220 @@
 ; it should be possible to concat these "value"-fields no? [] did not work :'(
 
 ; Unquoted strings
-(_
-  (_
-    (block_mapping_pair .
-      key: (flow_node
-        (plain_scalar
-          (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content
+              (#set! injection.language "bash")
+            )
+          )
         )
       )
-      value: (flow_node
-        (plain_scalar
-          (string_scalar) @injection.content
-          (#set! injection.language "bash")
-        )
-      )
-    ) .
+    )
   )
 )
 
-; Double- and single qouted strings
-(_
-  (_
-    (block_mapping_pair .
-      key: (flow_node
-        (plain_scalar
-          (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+; Unquoted strings (with shell.cmd / command.cmd)
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (block_node
+            (block_mapping
+              (block_mapping_pair
+                key: (flow_node
+                  (plain_scalar
+                    (string_scalar) @_sub_key (#eq? @_sub_key "cmd")
+                  )
+                )
+                value: (flow_node
+                  (plain_scalar
+                    (string_scalar) @injection.content
+                    (#set! injection.language "bash")
+                  )
+                )
+              )
+            )
+          )
         )
       )
-      value: (flow_node
-        (double_quote_scalar) @injection.content
-        (#set! injection.language "bash")
-        (#offset! @injection.content 0 1 0 -1)
-      )
-    ) .
+    )
   )
 )
-(_
-  (_
-    (block_mapping_pair .
-      key: (flow_node
-        (plain_scalar
-          (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+
+
+; Double- and single qouted strings
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (flow_node
+            (double_quote_scalar) @injection.content
+            (#set! injection.language "bash")
+            (#offset! @injection.content 0 1 0 -1)
+          )
         )
       )
-      value: (flow_node
-        (single_quote_scalar) @injection.content
-        (#set! injection.language "bash")
-        (#offset! @injection.content 0 1 0 -1)
+    )
+  )
+)
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (flow_node
+            (single_quote_scalar) @injection.content
+            (#set! injection.language "bash")
+            (#offset! @injection.content 0 1 0 -1)
+          )
+        )
       )
-    ) .
+    )
+  )
+)
+
+; Double- and single qouted strings (with shell.cmd / command.cmd)
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (block_node
+            (block_mapping
+              (block_mapping_pair
+                key: (flow_node
+                  (plain_scalar
+                    (string_scalar) @_sub_key (#eq? @_sub_key "cmd")
+                  )
+                )
+                value: (flow_node
+                  (double_quote_scalar) @injection.content
+                  (#set! injection.language "bash")
+                  (#offset! @injection.content 0 1 0 -1)
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+)
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (block_node
+            (block_mapping
+              (block_mapping_pair
+                key: (flow_node
+                  (plain_scalar
+                    (string_scalar) @_sub_key (#eq? @_sub_key "cmd")
+                  )
+                )
+                value: (flow_node
+                  (single_quote_scalar) @injection.content
+                  (#set! injection.language "bash")
+                  (#offset! @injection.content 0 1 0 -1)
+                )
+              )
+            )
+          )
+        )
+      )
+    )
   )
 )
 
 ; Support "|" and ">" -- note; hackishly works with ">-" and "|-"
-(_
-  (_
-    (block_mapping_pair .
-      key: (flow_node
-        (plain_scalar
-          (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "bash")
+            (#offset! @injection.content 0 1 0 0)
+          )
         )
       )
-      value: (block_node
-        (block_scalar) @injection.content
-        (#set! injection.language "bash")
-        (#offset! @injection.content 0 1 0 0)
+    )
+  )
+)
+
+; Support "|" and ">" -- note; hackishly works with ">-" and "|-" (with shell.cmd / command.cmd)
+(block_sequence
+  (block_sequence_item
+    (_
+      (_
+        (block_mapping_pair
+          key: (flow_node
+            (plain_scalar
+              (string_scalar) @_key (#any-of? @_key "command" "shell" "ansible.builtin.command" "ansible.builtin.shell")
+            )
+          )
+          value: (block_node
+            (block_mapping
+              (block_mapping_pair
+                key: (flow_node
+                  (plain_scalar
+                    (string_scalar) @_sub_key (#eq? @_sub_key "cmd")
+                  )
+                )
+                value: (block_node
+                  (block_scalar) @injection.content
+                  (#set! injection.language "bash")
+                  (#offset! @injection.content 0 1 0 0)
+                )
+              )
+            )
+          )
+        )
       )
-    ) .
+    )
   )
 )
