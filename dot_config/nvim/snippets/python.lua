f74a2154af6e4324c36e64e600f3a0cf3aa4d911
Magnus Larsen <magnusfynbo@hotmail.com>
2024-06-11 09:57:28 +0200

    (nvim/snippets): added python snippet for __init__

--- dot_config/nvim/snippets/python.lua
+++ dot_config/nvim/snippets/python.lua
@@ -14,6 +14,43 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
+-- adapted from: https://github.com/L3MON4D3/LuaSnip/wiki/Cool-Snippets#init-function-with-dynamic-initializer-list
+local function py_init()
+	return sn(
+		nil,
+		c(1, {
+			t(""),
+			sn(1, {
+				t(", "),
+				i(1),
+				d(2, py_init),
+			}),
+		})
+	)
+end
+
+-- splits the string of the comma separated argument list into the arguments
+-- and returns the text-/insert- or restore-nodes
+local function to_init_assign(args)
+	local tab = {}
+	local a = args[1][1]
+	if #a == 0 then
+		table.insert(tab, t({ "", "\tpass" }))
+	else
+		local cnt = 1
+		for e in string.gmatch(a, " ?([^,]*) ?") do
+			if #e > 0 then
+				table.insert(tab, t({ "", "\tself." }))
+				table.insert(tab, i(nil, e))
+				table.insert(tab, t(" = "))
+				table.insert(tab, t(e))
+				cnt = cnt + 1
+			end
+		end
+	end
+	return sn(nil, tab)
+end
+
 -- Snippets go here
 return {
 	s(
@@ -34,5 +71,12 @@ if __name__ == "__main__":
 				rep(1),
 			}
 		)
+	),
+	s(
+		"pyinit",
+		fmt([[def __init__(self{}):{}]], {
+			d(1, py_init),
+			d(2, to_init_assign, { 1 }),
+		})
 	),
 }
