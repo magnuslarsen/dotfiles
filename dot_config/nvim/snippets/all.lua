209b813cab08e2a1f674a40e11cf64e75f3159f1
Magnus Larsen <magnusfynbo@hotmail.com>
2024-06-12 10:59:12 +0200

    (nvim/snippets): use treesitter for shebang query; also add dressing.nvim

--- dot_config/nvim/snippets/all.lua
+++ dot_config/nvim/snippets/all.lua
@@ -14,12 +14,38 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
+local ft_to_shebang = {
+	sh = "bash",
+	python = "python3",
+}
+
+local function get_filetype()
+	-- get_parser errors if parser not present (no grammar for language).
+	local has_parser, parser = pcall(vim.treesitter.get_parser)
+
+	if has_parser then
+		local cursor = vim.api.nvim_win_get_cursor(0)
+		local lang = parser
+			:language_for_range({
+				cursor[1],
+				cursor[2],
+				cursor[1],
+				cursor[2],
+			})
+			:lang()
+
+		return ft_to_shebang[lang] or lang
+	else
+		return vim.bo.filetype
+	end
+end
+
 -- Snippets go here
 return {
 	s("shebang", {
 		t("#!/usr/bin/env "),
 		d(1, function()
-			return sn(nil, t({ vim.bo.filetype, "", "", "" }))
+			return sn(nil, t({ get_filetype(), "" }))
 		end),
 	}),
 }
