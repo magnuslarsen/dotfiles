4794d1a8a4a8a9b5e4e0b8273927e1237ce03082
Magnus Larsen <magnusfynbo@hotmail.com>
2024-07-23 07:40:31 +0200

    fix(treesitter): powershell is now supported by upstream

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -76,6 +76,7 @@ return {
 				"make",
 				"markdown",
 				"markdown_inline",
+				"powershell",
 				"printf",
 				"python",
 				"query",
@@ -101,17 +102,6 @@ return {
 		},
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
-
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.powershell = {
-				install_info = {
-					url = "https://github.com/airbus-cert/tree-sitter-powershell",
-					branch = "main",
-					files = { "src/parser.c", "src/scanner.c" },
-				},
-				filetype = "ps1",
-				used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" },
-			}
 		end,
 	},
 }
