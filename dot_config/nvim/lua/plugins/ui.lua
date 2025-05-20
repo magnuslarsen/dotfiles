f785d2e9a7a1fc8272f56083b7821e5fee0613d2
Magnus Larsen <magnusfynbo@hotmail.com>
2025-05-20 07:54:14 +0200

    fix: always load gitsigns

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -2,6 +2,7 @@ return {
 	{
 		-- Adds git releated signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
+		lazy = false,
 		keys = {
 			{
 				"<leader>hd",
