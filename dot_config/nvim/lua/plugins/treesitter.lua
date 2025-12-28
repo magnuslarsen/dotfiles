e6cd90b77573b89ff1b5f64f3bb187d56dc090a7
Magnus Larsen <magnusfynbo@hotmail.com>
2025-12-28 20:03:00 +0100

    refactor: treesitter main branch - simplify+auto install

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -57,30 +57,20 @@ return {
 				"yaml",
 			}
 
-			local isnt_installed = function(lang)
-				return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
-			end
+			ts.install(languages, { max_jobs = 4 })
 
-			local to_install = vim.tbl_filter(isnt_installed, languages)
-			if #to_install > 0 then
-				ts.install(to_install)
-			end
-
-			-- Enable tree-sitter after opening a file for a target language
-			local filetypes = {}
-			for _, lang in ipairs(languages) do
-				for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-					table.insert(filetypes, ft)
-				end
-			end
-			local ts_start = function(ev)
-				vim.treesitter.start(ev.buf)
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end
-
-			local gr = vim.api.nvim_create_augroup("TreeSitterAutoSetup", { clear = true })
-			local ts_opts = { group = gr, pattern = filetypes, callback = ts_start, desc = "Start tree-sitter" }
-			vim.api.nvim_create_autocmd("FileType", ts_opts)
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "Auto-start treesitter",
+				group = vim.api.nvim_create_augroup("treesitter-auto-start", { clear = true }),
+				callback = function(event)
+					if vim.tbl_contains(ts.get_available(), event.match) then
+						local lang = vim.treesitter.language.get_lang(event.match) or event.match
+						ts.install(lang):wait(5000)
+						vim.treesitter.start(event.buf)
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
 		end,
 	},
 }
