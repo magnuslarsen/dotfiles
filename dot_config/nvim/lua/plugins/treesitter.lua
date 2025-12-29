929375d3897187357571ef0b034e804be0d8730c
Magnus Larsen <magnusfynbo@hotmail.com>
2025-12-29 07:35:22 +0100

    fix: cleanup list of treesitter languages; we don't need that many initially

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -6,65 +6,51 @@ return {
 		lazy = false,
 		config = function()
 			local ts = require("nvim-treesitter")
+			local available_languages = ts.get_available()
 			local languages = {
 				"awk",
 				"bash",
-				"c",
 				"comment",
-				"cpp",
-				"css",
 				"csv",
 				"diff",
 				"dockerfile",
 				"fish",
-				"git_config",
 				"gitcommit",
 				"gitignore",
 				"go",
-				"gomod",
 				"html",
-				"htmldjango",
 				"ini",
-				"javascript",
 				"jq",
 				"json",
 				"jsonc",
 				"just",
 				"lua",
-				"make",
 				"markdown",
 				"markdown_inline",
-				"pem",
 				"powershell",
 				"printf",
 				"python",
 				"query",
 				"regex",
-				"requirements",
-				"rst",
 				"ruby",
 				"rust",
 				"sql",
-				"ssh_config",
 				"templ",
-				"tmux",
 				"toml",
 				"twig",
-				"typescript",
-				"vim",
 				"vimdoc",
 				"xml",
 				"yaml",
 			}
 
-			ts.install(languages, { max_jobs = 4 })
+			ts.install(languages, { max_jobs = 8 })
 
 			vim.api.nvim_create_autocmd("FileType", {
 				desc = "Auto-start treesitter",
 				group = vim.api.nvim_create_augroup("treesitter-auto-start", { clear = true }),
 				callback = function(event)
 					local lang = vim.treesitter.language.get_lang(event.match) or event.match
-					if vim.tbl_contains(ts.get_available(), lang) then
+					if vim.tbl_contains(available_languages, lang) then
 						ts.install(lang):wait(5000)
 						vim.treesitter.start(event.buf)
 						vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
